f013bc5f6cb727aca0b1023c783cb01d5b50b156
yoonju88 <yoonju.t@gmail.com>
2025-05-06 12:50:46 +0200

    Added variant on Badge shadcn/ui

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -17,6 +17,8 @@ const badgeVariants = cva(
           "border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
+        success: "bg-green-600 text-white border-transparent",
+        primary: "bg-sky-600 text-white border-transparent",
       },
     },
     defaultVariants: {

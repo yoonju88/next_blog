c199e81bda33609f0d04da517692a9a025429f7b
yoonju88 <yoonju.t@gmail.com>
2025-05-06 16:04:43 +0200

    Deleted unused files

--- app/admin-dashboard/edit/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function EditProperty() {
-    return (
-        <div>
-            Edit property
-        </div>
-    )
-}
--- app/to-do-list/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ToDoListPage() {
-    return (
-        <div>
-            ToDoListPage
-        </div>
-    )
-}

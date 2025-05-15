4c60ee08fe4b4b4df71840747b913fb97741161c
yoonju88 <yoonju.t@gmail.com>
2025-05-15 15:59:31 +0200

    Added the path on application

---
+++ app/my-favourites/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MyFavourites() {
+    return (
+        <div>
+            My Favourites Lists
+        </div>
+    )
+}
---
+++ app/reviews/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import EmtyList from '@/components/home/EmtyList'
+
+export default function ReviewsPage() {
+
+
+    return (
+        <div>
+            <EmtyList />
+
+        </div>
+    )
+}

04fd91a72d47d9726cec221c9182a787a01b4ce4
yoonju88 <yoonju.t@gmail.com>
2025-05-19 15:26:00 +0200

    added action deleteUserFavourites

---
+++ app/account/action.ts
@@ -0,0 +1,12 @@
+"use server"
+import { auth, firestore } from "@/firebase/server"
+import { cookies } from "next/headers"
+
+export const deleteUserFavourites = async () => {
+    const cookieStore = await cookies()
+    const token = cookieStore.get("firebaseAuthToken")?.value;
+
+    if (!token) { return }
+    const decodedToken = await auth.verifyIdToken(token)
+    await firestore.collection("favourites").doc(decodedToken.uid).delete()
+}

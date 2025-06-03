70f6238f54b7a23e67e755e2e6e8e9499993de7a
yoonju88 <yoonju.t@gmail.com>
2025-06-03 15:27:28 +0200

    Added home banner component

---
+++ components/home-banner/Banners.tsx
@@ -0,0 +1,45 @@
+'use client'
+import BannerCarousel from '@/components/home-banner/BannerCarousel';
+import { useEffect, useState } from 'react'
+
+export default function BannersClient({
+    webImages, mobileImages
+}: {
+    webImages: string[];
+    mobileImages: string[];
+}) {
+    const [isMobile, setIsMobile] = useState<boolean | null>(null);
+
+    useEffect(() => {
+        const checkIsMobile = () => {
+            setIsMobile(window.innerWidth <= 768); // 기준은 자유롭게
+        };
+
+        checkIsMobile();
+        window.addEventListener('resize', checkIsMobile);
+
+        return () => window.removeEventListener('resize', checkIsMobile);
+    }, []);
+    if (isMobile === null) return null; // 로딩 중 처리
+
+
+    return (
+        <>
+            {!isMobile &&
+                <BannerCarousel
+                    images={webImages}
+                    size="(max - width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    width={1290}
+                    height={600}
+                />
+            }
+            {isMobile &&
+                <BannerCarousel
+                    images={mobileImages}
+                    size="(max - width: 768px) 100vw, 50vw, 33vw"
+                    width={800}
+                    height={600}
+                />}
+        </>
+    )
+}

6807fae596970bbeff9f64483d9f2c01c90a9eb6
yoonju88 <yoonju.t@gmail.com>
2025-06-03 15:28:29 +0200

    change the structure get banners data

--- app/admin-dashboard/banners/page.tsx
+++ app/admin-dashboard/banners/page.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Breadcrumbs } from '@/components/ui/breadcrumb'
-import BannersTable from './banners-table'
+import BannersList from './banner-list'
+import { getWebBanners, getMobileBanners, getAllBanners } from './action';
+import { Edit2 } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
+export default async function BannerPage() {
+    const webImages = await getWebBanners();
+    const allWebImages = webImages.flatMap(web => web.images ?? [])
+    const mobileImages = await getMobileBanners();
+    const allMobileImages = mobileImages.flatMap(mobile => mobile.images ?? [])
 
-export default function BannerPage() {
+    const BannerId = webImages[0]?.id;
+    // console.log("id", firstBannerId)
+
     return (
         <>
             <Breadcrumbs items={[
@@ -16,7 +27,18 @@ export default function BannerPage() {
                 },
             ]}
             />
-            <BannersTable />
+            <div className="flex items-center justify-between mt-14">
+                <h1 className="text-3xl font-black uppercase text-foreground "> Banner Images manage</h1>
+                <Button>
+                    <Link href={`/admin-dashboard/banners/${BannerId}`}>
+                        <Edit2 /> Modifier Banner Images
+                    </Link>
+                </Button>
+            </div >
+            <BannersList
+                webImages={allWebImages}
+                mobileImages={allMobileImages}
+            />
         </>
     )
 }

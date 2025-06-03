d3867e5db88db6dab41c4a6ad9d5f501db5fafc5
yoonju88 <yoonju.t@gmail.com>
2025-06-03 15:29:08 +0200

    changed the name of bannerstable to banner list

---
+++ app/admin-dashboard/banners/banner-list.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+import BannerCarousel from '@/components/home-banner/BannerCarousel';
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { PlusCircleIcon } from 'lucide-react'
+
+export default async function BannersList({
+    webImages,
+    mobileImages,
+}: {
+    webImages: string[]
+    mobileImages: string[]
+}) {
+
+    return (
+        <>
+            <div className="mt-10">
+                <h1 className="mb-10 text-2xl text-primary font-bold uppercase">Web Banner Images</h1>
+                {!!webImages && (
+                    <BannerCarousel
+                        images={webImages}
+                        size="(max - width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                        width={1290}
+                        height={600}
+                    />
+                )}
+                <div className='border-t-1 border-primary/50 pb-12 mt-14' />
+                <div className="flex items-center justify-center flex-col mb-14">
+                    <h1 className="mb-10 text-2xl text-primary font-bold uppercase">Mobile Banner Images</h1>
+                    <div className="w-xl h-xl">
+                        {!!mobileImages && (
+                            <BannerCarousel
+                                images={mobileImages}
+                                size="(max - width: 768px) 100vw, 50vw, 33vw"
+                                width={800}
+                                height={600}
+                            />
+                        )}
+                    </div>
+                </div>
+                <div className='border-t-1 border-primary/50' />
+                {!webImages && !mobileImages && (
+                    <Button
+                        asChild
+                        className="flex-inline gap-2"
+                    >
+                        <Link href="/admin-dashboard/banners/new-banner">
+                            <PlusCircleIcon />New Banner
+                        </Link>
+                    </Button>
+                )}
+            </div>
+        </>
+    )
+}

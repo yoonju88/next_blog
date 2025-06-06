117b1a24fbcfd0b1960c9e73b126d8df45c74782
yoonju88 <yoonju.t@gmail.com>
2025-06-06 15:14:21 +0200

    Added footer structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Providers from "@/components/providers"
 import Nav from "@/components/navbar/nav";
 import { Toaster } from '@/components/ui/sonner';
 import { AuthProvider } from "@/context/auth";
+import Footer from "@/components/home/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
               {children}
               <Toaster />
             </main>
+            <Footer />
           </Providers>
         </AuthProvider>
       </body>

fe8f2f10d1c7a00f9da8cfcdaa67f89c3930a483
yoonju88 <yoonju.t@gmail.com>
2025-06-13 08:57:03 +0200

    send to login page if user is'nt existe

--- components/property/property-actions.tsx
+++ components/property/property-actions.tsx
@@ -5,6 +5,8 @@ import { useCart } from "@/context/cart-context"
 import { Property } from "@/types/property"
 import { toast } from "sonner"
 import numeral from "numeral"
+import { useAuth } from "@/context/auth"
+import { useRouter } from "next/navigation"
 
 type Props = {
     property: Property
@@ -12,10 +14,20 @@ type Props = {
 
 export default function PropertyActions({ property }: Props) {
     const { addToCart } = useCart()
+    const { user } = useAuth()
+    const router = useRouter()
 
     const handleAddToCart = () => {
+        if (!user) {
+            toast.error("Login Required", {
+                description: "Please login to add items to your cart."
+            })
+            router.push('/login')
+            return
+        }
+
         addToCart(property)
-        toast.success("Added to cart", {
+        toast.success("Added to Cart", {
             description: "The item has been added to your cart."
         })
     }

8a09b8dcd3bd25ab07021e2dcd11890660ffb89f
yoonju88 <yoonju.t@gmail.com>
2025-06-16 14:54:00 +0200

    Updated items space

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
       />
       <section className="w-full">
         <h1 className="text-foreground text-4xl text-center font-bold">New Arrivals</h1>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-14'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 mt-14'>
           {recentItems.map((item) => (
             <PropertyCard
               key={item.id}

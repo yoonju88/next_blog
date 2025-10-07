aadec38ec65d9a39f20c2a755126f0ee41e9d7e2
yoonju88 <yoonju.t@gmail.com>
2025-10-07 15:32:00 +0200

    Create ItemSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { getWebBanners, getMobileBanners } from "@/app/admin-dashboard/banners/action";
 import BannersClient from '@/components/home-banner/Banners';
+import ItemSection from "@/components/home/ItemSection";
 import PropertyCard from "@/components/property/PropertyCard";
 import { getOnSaleProperties, getRecentProperties } from "@/lib/properties";
 import Link from "next/link";
-import { ChevronRight } from 'lucide-react';
+
 
 export default async function Home() {
   const webImages = await getWebBanners();
@@ -26,56 +27,21 @@ export default async function Home() {
         webImages={allWebImages}
         mobileImages={allMobileImages}
       />
-      <section className="w-full">
+      <section>
         <h1 className="text-foreground text-4xl text-center font-bold">
           <Link href="/property?sort=newest">
             New Arrivals
           </Link>
         </h1>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 mt-20'>
-          {recentItems.map((item) => (
-            <PropertyCard
-              key={item.id}
-              property={item}
-              actionButton={
-                <button
-                  type="button"
-                  className={`shrink-0 rounded-2xl bg-primary/10 p-1 hover:bg-primary/20 hover:shadow-sm ${hoverEffect}`}
-                >
-                  <Link href={`/property/${item.id}`}>
-                    <ChevronRight className="w-5 h-5" />
-                  </Link>
-                </button>
-              }
-            />
-          ))
-          }
-        </div >
+        <ItemSection data={recentItems} />
       </section>
       <section>
         <h1 className="text-foreground text-4xl text-center font-bold">
           <Link href="/property?sale=true">
             On sale
-          </Link></h1>
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 mt-20'>
-          {saleItems.map((item) => (
-            <PropertyCard
-              key={item.id}
-              property={item}
-              actionButton={
-                <button
-                  type="button"
-                  className={`shrink-0 rounded-2xl bg-primary/10 p-1 hover:bg-primary/20 hover:shadow-sm ${hoverEffect}`}
-                >
-                  <Link href={`/property/${item.id}`}>
-                    <ChevronRight className="w-5 h-5" />
-                  </Link>
-                </button>
-              }
-            />
-          ))
-          }
-        </div>
+          </Link>
+        </h1>
+        <ItemSection data={saleItems} />
       </section >
     </div >
   );
---
+++ components/home/ItemSection.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { ChevronRight } from 'lucide-react';
+import PropertyCard from '../property/PropertyCard';
+import Link from 'next/link';
+import { Property } from '@/types/property';
+
+
+interface itemsProps {
+    data: Property[];
+}
+
+export default function ItemSection({ data }: itemsProps) {
+    if (!data) {
+        return null
+    }
+    const hoverEffect = "transition-all duration-300"
+
+    return (
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-2 mt-20'>
+            {data.map((item) => (
+                <PropertyCard
+                    key={item.id}
+                    property={item}
+                    actionButton={
+                        <button
+                            type="button"
+                            className={`shrink-0 rounded-2xl bg-primary/10 p-1 hover:bg-primary/20 hover:shadow-sm ${hoverEffect}`}
+                        >
+                            <Link href={`/property/${item.id}`}>
+                                <ChevronRight className="w-5 h-5" />
+                            </Link>
+                        </button>
+                    }
+                />
+            ))
+            }
+        </div >
+    )
+}

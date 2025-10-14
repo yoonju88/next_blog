c6ef84b999d9818e263e4143c8b3e8cbf334c0a2
yoonju88 <yoonju.t@gmail.com>
2025-10-14 11:43:12 +0200

    Error: Unexpected var, use let or const instead.  no-var

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-    var prisma: PrismaClient | undefined;
-}
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 

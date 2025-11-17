35a23dad2c203ac46662269d156e0fa6888982dd
yoonju88 <yoonju.t@gmail.com>
2025-11-17 15:20:04 +0100

    Update footer

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -4,13 +4,17 @@ import React from 'react'
 export default function Footer() {
 
     return (
-        <div className="text-foreground mt-36">
-            <div className="bg-gray-100">
-                <div className="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
-                    <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">© 2020 Tailblocks —
-                    </p>
-                </div>
+        <footer className="bg-gray-100 mt-36">
+            <div className="px-8 py-6 flex flex-col lg:flex-row items-center">
+                <h1 className='text-3xl text-foreground font-bold'>
+                    <Link href="/">
+                        CosCorée
+                    </Link>
+                </h1>
+                <p className="text-sm text-foreground/60 lg:text-left text-center sm:ml-6 mt-4 lg:mt-0">
+                    ©2025 All right reserved. Designed and developped by TERRENOIRE Yoonju.
+                </p>
             </div>
-        </div >
+        </footer>
     )
 }

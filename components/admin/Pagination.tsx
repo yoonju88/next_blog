f955a56735ac3636d2493d405fcef76a53aacf07
yoonju88 <yoonju.t@gmail.com>
2025-11-21 12:54:00 +0100

    Update pagination style

--- components/admin/Pagination.tsx
+++ components/admin/Pagination.tsx
@@ -15,24 +15,25 @@ export default function Pagination({ currentPage, totalPages, goToPage }: Pagina
             <Button
                 onClick={() => goToPage(currentPage - 1)}
                 disabled={currentPage === 1}
-                className="px-2 py-1 rounded border disabled:opacity-50"
+                className="px-2 py-1 rounded border bg-foreground/5 disabled:opacity-50"
+                variant="default"
             >
                 <ChevronLeftIcon />
 
             </Button>
 
             {Array.from({ length: totalPages }, (_, i) => i + 1).map(page => (
-                <button
+                <Button
                     key={page}
                     onClick={() => goToPage(page)}
-                    className={`px-3 py-1 rounded border ${currentPage === page ? 'bg-blue-500 text-white' : 'bg-white'}`}
-                >{page}</button>
+                    className={`px-3 py-1  ${currentPage === page ? 'bg-foreground/15' : 'bg-foreground/5'}`}
+                >{page}</Button>
             ))}
 
             <Button
                 onClick={() => goToPage(currentPage + 1)}
                 disabled={currentPage === totalPages}
-                className="px-3 py-1 rounded border bg-gray-100 disabled:opacity-50"
+                className="px-3 py-1 bg-foreground/5 disabled:opacity-50"
             >
                 <ChevronRightIcon />
             </Button>
